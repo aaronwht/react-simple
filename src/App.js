532f9ebe8e339064cf2af643dc295bbc51f8fa14
Aaron White <8147079+aaronwht@users.noreply.github.com>
2019-07-24 16:23:37 -0700

    Updated App.js
    Added comments

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-// src/App.js - displays API content
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+// src/App.js
+import React, { useState, useEffect } from 'react' // Imports React and the useState and useEffect hooks
+import axios from 'axios' // Imports axios, the promise-based library used to retreive JSON data
 
+// Exporting the app module as the default
 export default () => {
+   // Establishing and destructuring the state tuple
    const [books, setBooks] = useState([])
    
+   // Executes the useEffect hook
    useEffect(() => {
+    // Creates an anonymous function
     (async () => {
+      // Makes promise-based request and destructures the response
       const { data: { books } } = await axios.get('/books.json')
+          // Sets the book state
 	  setBooks(books)
-    })()
-  }, [])
+    })() // Immediately Invoked Function Expression (IIFE)
+  }, []) // Empty dependency array ensures the Effect is only invoked upon initial render
   
    return (
-      <>
+      <> // Syntactic Sugar for <React.Fragment>
+      	 // Truthy statement ensuring books exists, then iterates over the array
          {books && books.map(book => {
             return (
               <div key={book._id}>
